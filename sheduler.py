4eddc37e170c134a20fe960edb679f3c4d004e8b
MaximSava <39280033+MaximSava@users.noreply.github.com>
2021-08-03 15:42:13 +0300

    changed triger time

--- sheduler.py
+++ sheduler.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     schedule.every().monday.at("12:45").do(sf.send_form_setup)
     schedule.every().monday.at("15:00").do(sf.send_form_setup)
     schedule.every().tuesday.at("13:00").do(sf.send_form_setup)
-    schedule.every().tuesday.at("15:00").do(sf.send_form_setup)
+    schedule.every().tuesday.at("15:50").do(sf.send_form_setup)
     schedule.every().wednesday.at("12:45").do(sf.send_form_setup)
     schedule.every().wednesday.at("15:00").do(sf.send_form_setup)
     schedule.every().thursday.at("13:45").do(sf.send_form_setup)
