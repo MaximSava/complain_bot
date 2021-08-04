2543f065e54e3046870c09a05bcb396973bbc4cf
MaximSava <39280033+MaximSava@users.noreply.github.com>
2021-08-04 23:34:37 +0300

    Test parking_bot

--- parking_bot.py
+++ parking_bot.py
@@ -143,7 +143,7 @@ class Get_Data:
         capabilities = webdriver.DesiredCapabilities.FIREFOX
         capabilities['marionette'] = True
         
-        binary = FirefoxBinary("FIREFOX_BIN")
+        binary = FirefoxBinary("firefox")
         #binary = FirefoxBinary(os.environ.get("FIREFOX_BIN"))
         #binary = FirefoxBinary(r"C:\Users\макс\AppData\Local\Mozilla Firefox\firefox.exe")
         options = Options()
